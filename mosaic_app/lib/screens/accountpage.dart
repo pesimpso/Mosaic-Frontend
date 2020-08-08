2136d94f3ac7c2593a9cb9006ad9fd111d9a3390
simpson-peter <simpson.petere@gmail.com>
2020-08-07 23:36:32 -0400

    fixed account screen rendering on ios

--- mosaic_app/lib/screens/accountpage.dart
+++ mosaic_app/lib/screens/accountpage.dart
@@ -16,80 +16,75 @@ class _AccountPageState extends State<AccountPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Row(
-              children: <Widget>[
-                //caret icon
-                CaretIcon(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width - 235,
-                ),
-                Text(
-                  'Account',
-                  style: kTitleStyle,
-                  textAlign: TextAlign.right,
-                )
-              ]
-            ),
-            Divider(
-              color: Colors.white,
-              thickness: 5,
-            ),
+        body: Column(children: [
+          Row(children: <Widget>[
+            //caret icon
+            CaretIcon(),
             SizedBox(
-              height: 30,
+              width: MediaQuery.of(context).size.width - 235,
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 15.0, right: 15.0),
-              child: Container(
-                height: MediaQuery.of(context).size.height - 153,
-                width: MediaQuery.of(context).size.width - 30,
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-                      child: 
-                      Row(
-                        children: [
+            Text(
+              'Account',
+              style: kTitleStyle,
+              textAlign: TextAlign.right,
+            )
+          ]),
+          Divider(
+            color: Colors.white,
+            thickness: 5,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: Container(
+              height: MediaQuery.of(context).size.height - 153,
+              width: MediaQuery.of(context).size.width - 30,
+              decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                    child: Row(
+                      children: [
                         Text(
                           'Personal Information',
                           style: kHeaderStyle,
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Divider(
-                      color: Colors.black,
-                      thickness: 1,
-                    ),
-                    Text(
-                      'Username: maryowen',
-                      style: kBodyStyleDark,
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      'Password: password',
-                      style: kBodyStyleDark,
-                      textAlign: TextAlign.center,
-                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  Text(
+                    'Username: maryowen',
+                    style: kBodyStyleDark,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Password: password',
+                    style: kBodyStyleDark,
+                    textAlign: TextAlign.center,
+                  ),
                   Divider(
                     color: Colors.black,
                     thickness: 1,
                   ),
                   Row(
                     children: [
-                    Container(
-                      child: HeartIcon(),
-                      width: 60,
-                    ),
-                    Text(
-                      'Favorites',
-                      style: kHeaderStyle,
+                      Container(
+                        child: HeartIcon(),
+                        width: 60,
+                      ),
+                      Text(
+                        'Favorites',
+                        style: kHeaderStyle,
                       ),
                     ],
                   ),
@@ -98,7 +93,7 @@ class _AccountPageState extends State<AccountPage> {
                     thickness: 1,
                   ),
                   /* BEGIN FAVORITES LIST */
-                  /* NEED TO ADD FUNCTIONALITY. LOOP THROUGH LIST AND GENERATE 
+                  /* NEED TO ADD FUNCTIONALITY. LOOP THROUGH LIST AND GENERATE
                   CARDS ACCORDINGLY */
                   Container(
                     height: 270,
@@ -110,28 +105,32 @@ class _AccountPageState extends State<AccountPage> {
                               //TODO Delete debugPrint and implement functionality
                               debugPrint("Restaurant button pressed");
                             },
-                            child: 
-                              Row(children: <Widget>[
+                            child: Row(
+                              children: <Widget>[
                                 Container(
                                   height: 40,
                                   width: 40,
                                   margin: EdgeInsets.all(5),
-                                  child: Image.asset('assets/images/gray_square.png'),
+                                  child: Image.asset(
+                                      'assets/images/gray_square.png'),
                                 ),
                                 Text(
-                                  'Name\n', 
-                                  style: kBodyStyle,   
-                                  textAlign: TextAlign.left,                   
+                                  'Name\n',
+                                  style: kBodyStyle,
+                                  textAlign: TextAlign.left,
                                 ),
-                              ],),
+                              ],
+                            ),
                           ),
                           color: Colors.black,
-                          margin: EdgeInsets.only(bottom: 10, left: 30, right: 30),
+                          margin:
+                              EdgeInsets.only(bottom: 10, left: 30, right: 30),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(5),
                           ),
                         ),
-                    ],),
+                      ],
+                    ),
                   ),
                   SizedBox(height: 8),
                   Divider(
@@ -139,38 +138,34 @@ class _AccountPageState extends State<AccountPage> {
                     thickness: 1,
                   ),
                   Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: 
-                      Row(
-                        children: [
+                    padding: EdgeInsets.only(left: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
                         Text(
                           'Logout',
                           style: kHeaderStyle,
+                        ),
+                        MaterialButton(
+                          child: Icon(
+                            Icons.input,
                           ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width - 225,
-                          ),
-                          MaterialButton(
-                            child: Icon(
-                              Icons.input,
-                            ),
-                            onPressed: () {
-                              //TODO implement functionality
-                              debugPrint("Logout button pressed");
-                            },
-                          ),
-                        ],
-                      ),
+                          onPressed: () {
+                            //TODO implement functionality
+                            debugPrint("Logout button pressed");
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 30,
-            ),
-          ]
-        ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ]),
       ),
     );
   }
