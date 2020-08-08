14cae5169944cee1eb172395dfb5caa0cc154e4d
simpson-peter <simpson.petere@gmail.com>
2020-08-08 14:42:46 -0400

    implemented geolocation

--- mosaic_app/lib/services/user_locator.dart
+++ mosaic_app/lib/services/user_locator.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
 
 class UserLocator {
   double userLongitude;
   double userLatitude;
   Position position;
+  Geolocator geolocator = Geolocator();
 
   UserLocator() {
     this.getCurrentLocation();
@@ -11,11 +13,11 @@ class UserLocator {
 
   Future<void> getCurrentLocation(
       {LocationAccuracy locAcc = LocationAccuracy.high}) async {
-    GeolocationStatus geolocationStatus =
-        await Geolocator().checkGeolocationPermissionStatus();
-
-    position = await Geolocator().getCurrentPosition(desiredAccuracy: locAcc);
+    position = await geolocator.getCurrentPosition(desiredAccuracy: locAcc);
     userLongitude = position.longitude;
     userLatitude = position.latitude;
+
+    debugPrint(userLatitude.toString());
+    debugPrint(userLongitude.toString());
   }
 }
