5e2ccbd6eacec791acaa7169c2edf2219dd661ff
simpson-peter <simpson.petere@gmail.com>
2020-08-08 01:12:50 -0400

    began provider incorporation

--- mosaic_app/lib/main.dart
+++ mosaic_app/lib/main.dart
@@ -6,7 +6,9 @@ import 'package:mosaicapp/screens/sign_up.dart';
 import 'package:mosaicapp/screens/settings.dart';
 import 'package:mosaicapp/screens/accountpage.dart';
 import 'package:mosaicapp/screens/results.dart';
+import 'package:provider/provider.dart';
 
+import 'models/app_data.dart';
 import 'models/restaurant.dart';
 
 void main() {
@@ -17,27 +19,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: BusinessPage.id,
-      //Route String : Function pair
-      routes: {
-        Login.id: (context) => Login(),
-        Homepage.id: (context) => Homepage(),
-        SignUp.id: (context) => SignUp(),
-        Settings.id: (context) => Settings(),
-        AccountPage.id: (context) => AccountPage(),
-        Results.id: (context) => Results(),
-        //TODO Drop the hard-coded restaurant instance here, figure out data interchange to pass it in
-        BusinessPage.id: (context) => BusinessPage(
-              restaurant: Restaurant(
-                name: 'Name',
-                distFromUser: 1.2,
-                rating: 3.5,
-                phone: "(123) 456-7890",
-                address: "123 Road St.",
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        initialRoute: BusinessPage.id,
+        //Route String : Function pair
+        routes: {
+          Login.id: (context) => Login(),
+          Homepage.id: (context) => Homepage(),
+          SignUp.id: (context) => SignUp(),
+          Settings.id: (context) => Settings(),
+          AccountPage.id: (context) => AccountPage(),
+          Results.id: (context) => Results(),
+          //TODO Drop the hard-coded restaurant instance here, figure out data interchange to pass it in
+          BusinessPage.id: (context) => BusinessPage(
+                restaurant: Restaurant(
+                  name: 'Name',
+                  distFromUser: 1.2,
+                  rating: 3.5,
+                  phone: "(123) 456-7890",
+                  address: "123 Road St.",
+                ),
               ),
-            ),
-      },
+        },
+      ),
     );
   }
 }
---
+++ mosaic_app/lib/models/app_data.dart
@@ -0,0 +1,4 @@
+//Data-only class used by provider to share data across the app
+import 'package:flutter/material.dart';
+
+class AppData extends ChangeNotifier {}
