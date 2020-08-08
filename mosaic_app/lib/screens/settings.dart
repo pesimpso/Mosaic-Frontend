f7374b1e2010bf10eeb6732f78ef0aa8da7b6c56
simpson-peter <simpson.petere@gmail.com>
2020-08-07 23:29:38 -0400

    fixed settings screen size on iOS

--- mosaic_app/lib/screens/settings.dart
+++ mosaic_app/lib/screens/settings.dart
@@ -15,95 +15,89 @@ class _SettingsState extends State<Settings> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Row(
-              children: <Widget>[
-                //caret icon
-                CaretIcon(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width - 235,
-                ),
-                Text(
-                  'Settings',
-                  style: kTitleStyle,
-                  textAlign: TextAlign.right,
-                )
-              ]
-            ),
-            Divider(
-              color: Colors.white,
-              thickness: 5,
-            ),
+        body: Column(children: [
+          Row(children: <Widget>[
+            //caret icon
+            CaretIcon(),
             SizedBox(
-              height: 30,
+              width: MediaQuery.of(context).size.width - 235,
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 15.0, right: 15.0),
-              child: Container(
-                height: MediaQuery.of(context).size.height - 153,
-                width: MediaQuery.of(context).size.width - 30,
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-                      child: 
-                      Row(
-                        children: [
+            Text(
+              'Settings',
+              style: kTitleStyle,
+              textAlign: TextAlign.right,
+            )
+          ]),
+          Divider(
+            color: Colors.white,
+            thickness: 5,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 15,
+                right: 15,
+              ),
+              width: double.infinity,
+              decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                    child: Row(
+                      children: [
                         Text(
                           'Account Settings',
                           style: kHeaderStyle,
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Divider(
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  /* BEGIN ACCOUNT BUTTONS */
+                  SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: RaisedButton(
+                      child: Text(
+                        'Edit username',
+                        style: kBodyStyle,
+                      ),
                       color: Colors.black,
-                      thickness: 1,
-                    ),
-                    /* BEGIN ACCOUNT BUTTONS */
-                    SizedBox(
-                      width: 150,
-                      height: 30,
-                      child: 
-                      RaisedButton(
-                        child: 
-                          Text(
-                            'Edit username', 
-                            style: kBodyStyle,                      
-                          ),
-                        color: Colors.black,
-                        clipBehavior: Clip.hardEdge,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                         ),
-                        /* NEED TO ADD FUNCTIONALITY */
-                        onPressed: () {
-                          //TODO Delete debugPrint and implement functionality
-                          debugPrint("Edit username button pressed");
-                        },
+                      clipBehavior: Clip.hardEdge,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                      /* NEED TO ADD FUNCTIONALITY */
+                      onPressed: () {
+                        //TODO Delete debugPrint and implement functionality
+                        debugPrint("Edit username button pressed");
+                      },
+                    ),
                   ),
                   SizedBox(height: 8),
                   SizedBox(
                     width: 150,
                     height: 30,
-                    child: 
-                    RaisedButton(
-                      child: 
-                        Text(
-                          'Edit password', 
-                          style: kBodyStyle,                      
-                        ),
+                    child: RaisedButton(
+                      child: Text(
+                        'Edit password',
+                        style: kBodyStyle,
+                      ),
                       color: Colors.black,
                       clipBehavior: Clip.hardEdge,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20),
-                        ),
+                      ),
                       /* NEED TO ADD FUNCTIONALITY */
                       onPressed: () {
                         //TODO Delete debugPrint and implement functionality
@@ -117,60 +111,55 @@ class _SettingsState extends State<Settings> {
                     thickness: 1,
                   ),
                   Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: 
-                      Row(
-                        children: [
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      children: [
                         Text(
                           'About',
                           style: kHeaderStyle,
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Divider(
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                  ),
+                  /* BEGIN ABOUT BUTTONS */
+                  SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: RaisedButton(
+                      child: Text(
+                        'Terms of Use',
+                        style: kBodyStyle,
+                      ),
                       color: Colors.black,
-                      thickness: 1,
-                    ),
-                    /* BEGIN ABOUT BUTTONS */
-                    SizedBox(
-                      width: 150,
-                      height: 30,
-                      child: 
-                      RaisedButton(
-                        child: 
-                          Text(
-                            'Terms of Use', 
-                            style: kBodyStyle,                      
-                          ),
-                        color: Colors.black,
-                        clipBehavior: Clip.hardEdge,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                         ),
-                        /* NEED TO ADD FUNCTIONALITY */
-                        onPressed: () {
-                          //TODO Delete debugPrint and implement functionality
-                          debugPrint("Terms of Use button pressed");
-                        },
+                      clipBehavior: Clip.hardEdge,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
+                      /* NEED TO ADD FUNCTIONALITY */
+                      onPressed: () {
+                        //TODO Delete debugPrint and implement functionality
+                        debugPrint("Terms of Use button pressed");
+                      },
+                    ),
                   ),
                   SizedBox(height: 8),
                   SizedBox(
                     width: 150,
                     height: 30,
-                    child: 
-                    RaisedButton(
-                      child: 
-                        Text(
-                          'Data Policy', 
-                          style: kBodyStyle,                      
-                        ),
+                    child: RaisedButton(
+                      child: Text(
+                        'Data Policy',
+                        style: kBodyStyle,
+                      ),
                       color: Colors.black,
                       clipBehavior: Clip.hardEdge,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20),
-                        ),
+                      ),
                       /* NEED TO ADD FUNCTIONALITY */
                       onPressed: () {
                         //TODO Delete debugPrint and implement functionality
@@ -181,24 +170,23 @@ class _SettingsState extends State<Settings> {
                   SizedBox(height: 200),
                   /* BEGIN DELETE ACCOUNT BUTTON */
                   SizedBox(
-                    width: 200,
+                    width: 250,
                     height: 50,
-                    child: 
-                    RaisedButton(
-                      child: 
-                        Text(
-                          'Delete Account', 
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                            fontFamily: 'Josefin Sans:wght500',
-                          ),                      
+                    child: RaisedButton(
+                      child: Text(
+                        'Delete Account',
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontFamily: 'Josefin Sans:wght500',
                         ),
+                      ),
                       color: Colors.black,
                       clipBehavior: Clip.hardEdge,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(45),
-                        ),
+                      ),
                       /* NEED TO ADD FUNCTIONALITY */
                       onPressed: () {
                         //TODO Delete debugPrint and implement functionality
@@ -206,15 +194,11 @@ class _SettingsState extends State<Settings> {
                       },
                     ),
                   ),
-                  ],
-                ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 30,
-            ),
-          ]
-        ),
+          ),
+        ]),
       ),
     );
   }
