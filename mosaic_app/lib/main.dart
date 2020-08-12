311927d546eef9afcdb1d591f45c9dde03dcc5f3
simpson-peter <simpson.petere@gmail.com>
2020-08-12 02:11:33 -0400

    alpha commit

--- mosaic_app/lib/main.dart
+++ mosaic_app/lib/main.dart
@@ -16,12 +16,6 @@ void main() {
   runApp(MyApp());
 }
 
-/*TODOs:
-* TODO: Prevent user from swiping left to pop Navigator and access older screens that should be inaccessible
-* TODO: Fix off-positioned elements on a few screens (results, businesspage)
-* TODO: Implement filter functionality with restaurant data on results screen
-* TODO: Fix distance slider filter on results screen
-*/
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -29,6 +23,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => AppData(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData.light(),
         initialRoute: Login.id,
         //Route String : Function pair
@@ -45,3 +40,10 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/*TODOs:
+* TODO: Prevent user from swiping left to pop Navigator and access older screens that should be inaccessible
+* TODO: Fix off-positioned elements on a few screens (results, businesspage)
+* TODO: Implement filter functionality with restaurant data on results screen
+* TODO: Fix distance slider filter on results screen
+*/
